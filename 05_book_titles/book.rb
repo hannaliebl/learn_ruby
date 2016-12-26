d18fd21ea936a3d9ab7955736452ab12bed77399
Hanna Liebl <hanna@radarfirst.com>
2016-12-26 13:47:20 -0800

    Add title case and timer exercises

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title
+    title_arr = []
+    words_arr = @title.split(" ")
+    title_arr.push(words_arr[0].capitalize)
+    words_arr.shift
+    words_arr.each do |word|
+      if word == 'the' || word == 'a' || word == 'an' || word == 'and' || word == 'of' || word == 'in'
+        title_arr.push(word)
+      else
+        title_arr.push(word.capitalize)
+      end
+    end
+    @title = title_arr.join(" ")
+  end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,27 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    convert(@seconds)
+  end
+
+  def convert sec
+    hours = pad(((sec / 60) / 60).to_s)
+    mins = pad(((sec / 60) % 60).to_s)
+    seconds = pad((sec % 60).to_s)
+    hours + ":" + mins + ":" + seconds
+  end
+
+  def pad number_string
+    if number_string. length == 1
+      "0" + number_string
+    else
+      number_string
+    end
+  end
+
 end
